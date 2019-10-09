f9446ffd402199de5fcc9ea5aa621b1089f6f315
John Hallat <jhallat@gmail.com>
2019-10-09 07:43:25 -0400

    started properties impl

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ use std::path::Path;
 fn main() {
 
     let properties = load_properties();
-    let data_home = properties.get(&String::from("DATA_HOME")); 
-    println!("{:?}", data_home);
+    let data_home = properties.get(&String::from("data.home")); 
+    match data_home {
+        Some(value) => println!("data.home = {}", value),
+        None => println!("I have no home")
+    }
     loop {
         display_prompt();
         let line = read_line();
@@ -58,4 +61,35 @@ fn load_properties() -> HashMap<String, String> {
     properties.insert(property_values[0].to_string(), property_values[1].to_string());
 
     properties
+}
+
+struct Properties {
+    property_map: HashMap<String, String>
+}
+
+impl Properties {
+   fn new() -> Properties {
+       Properties {
+           property_map: HashMap::new()
+       }
+   } 
+
+   fn load(&mut self) {
+       self.property_map.clear();
+       let contents = fs::read_to_string("listdb.properties")
+            .expect("I can read the properties file :-(");
+       let property_lines: Vec<&str> = contents.split('\n').collect();
+        for property_line in property_lines {
+            let property_values: Vec<&str> = contents.split('=').collect();
+            self.property_map.insert(property_values[0].to_string(), property_values[1].to_string());
+        }
+   }
+
+   fn get(&self, key: String) -> String {
+       match self.property_map.get(&key) {
+           Some(value) => value.to_string(),
+           None => "".to_string()
+       }
+       
+   }
 }
